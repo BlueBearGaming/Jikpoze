6afcbc360f512b90a2593efd2dc2fc621daf0db6
Vincent Chalnot <vchalnot@clever-age.com>
2014-06-02 14:39:24 +0200

    Added isometric board and tileset, wip

---
+++ build/web/lib/hexcell.dart
@@ -0,0 +1,35 @@
+part of jikpoze;
+
+class HexCell extends Cell {
+
+	HexCell(Board board, Point position, int size) : super(board, position, size);
+
+	void buildGraphics(Graphics g) {
+		int numberOfSides = 6; // hexagon
+		num a = Math.PI / 2;
+		num size = this.size / Math.cos(a + 2 * Math.PI / numberOfSides);
+		g.moveTo(size * Math.cos(a), size * Math.sin(a));
+		for (int i = 1; i <= numberOfSides; i++) {
+		    g.lineTo(size * Math.cos(a + i * 2 * Math.PI / numberOfSides), size * Math.sin(a + i * 2 * Math.PI / numberOfSides));
+		}
+	}
+
+	List<Point> getAdjacentPoints() {
+		num x = position.x;
+		num y = position.y;
+		List<Point> points = [
+			new Point(x + 1, y),
+			new Point(x - 1, y),
+			new Point(x, y + 1),
+			new Point(x, y - 1),
+		];
+		if(y.floor() % 2 == 0) {
+			points.add(new Point(x + 1, y - 1));
+			points.add(new Point(x + 1, y + 1));
+		} else {
+			points.add(new Point(x - 1, y - 1));
+			points.add(new Point(x - 1, y + 1));
+		}
+     	return points;
+	}
+}
---
+++ web/lib/isoboard.dart
@@ -0,0 +1,23 @@
+part of jikpoze;
+
+class IsoBoard extends Board {
+
+	IsoBoard(Html.CanvasElement canvas, int cellSize) : super(canvas, cellSize);
+
+	Cell createCell(point) {
+		return cells[point] = new IsoCell(this, point, cellSize);
+	}
+
+	Point gamePointToViewPoint(Point gamePoint){
+		num viewX = gamePoint.x * cellSize * 2;
+		num viewY = gamePoint.y * cellSize * IsoCell.skewFactor;
+		if(gamePoint.y.floor() % 2 == 0) {
+			viewX += cellSize;
+		}
+		return new Point(viewX, viewY);
+	}
+
+	Point viewPointToGamePoint(Point viewPoint){
+		return new Point((viewPoint.x / cellSize / 2).floor(), (viewPoint.y / cellSize / Math.cos(Math.PI/4) / 2).floor());
+	}
+}
