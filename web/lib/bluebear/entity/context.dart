329299be98e43d22ce401490c2b3f4cfc6f7d3c0
VincentChalnot <vincent.chalnot@gmail.com>
2015-03-25 23:50:11 +0100

    Fixing map loading on center coordinates 0,0 and passing userContext and loadContext

--- web/lib/bluebear/entity/context.dart
+++ web/lib/bluebear/entity/context.dart
@@ -2,11 +2,13 @@ part of bluebear;
 
 class Context {
     int id;
+    String label;
     Map map;
     List<MapItem> mapItems = new List<MapItem>();
 
     Context.fromJsonData(var data) {
         id = data['id'];
+        label = data['label'];
         map = new Map.fromJsonData(data['map']);
         if (null != data['mapItems']) {
             for (var mapItem in data['mapItems']) {
