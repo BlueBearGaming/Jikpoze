f505a8a629727a6bf8472c18028debd02181f924
Vincent Chalnot <vchalnot@clever-age.com>
2018-04-03 23:27:11 +0200

    Adding socket.io server side

--- web/main.dart
+++ web/main.dart
@@ -1,8 +1,19 @@
 import 'dart:html';
 import 'dart:convert';
+import 'package:socket_io/socket.io.dart';
 import 'lib/bluebear/bluebear.dart' as BlueBear;
 
 void main() {
+    var io = new Server();
+    io.on('connection', (Socket client) {
+        String hash = client.conn.hashCode;
+        print('Connection initiated : $hash');
+        client.on('bluebear.engine.clientUpdate', (data) {
+            client.emit('bluebear.engine.clientUpdate', data);
+        });
+    });
+    io.listen(3000);
+
     Element canvas = querySelector('#canvas_map');
     Map options = JSON.decode(canvas.attributes['data-options']);
     if (options.containsKey('edition') && true == options['edition']) {
